f8f2e56f836d88b64d1ce3377bc49e7d566436b2
brthanmathwoag <brthanmathwoag@tznvy.eu>
2016-12-18 22:26:30 +0100

    Forced key ordering when rendering tasks to yaml

--- jancy-transpiler/src/main/scala/jancy/transpiler/rendering/TaskRenderer.scala
+++ jancy-transpiler/src/main/scala/jancy/transpiler/rendering/TaskRenderer.scala
@@ -3,6 +3,7 @@ package jancy.transpiler.rendering
 import jancy.core.Task
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable.LinkedHashMap
 
 object TaskRenderer {
 
@@ -13,8 +14,12 @@ object TaskRenderer {
   def render(tasks: Seq[Task]): String =
     YamlContext.get.dump(tasks.map(buildModel(_).asJava).toArray)
 
-  def buildModel(task: Task): Map[String, Any] = {
+  def buildModel(task: Task): LinkedHashMap[String, Any] = {
     val taskArguments = task.getArguments.asScala.toMap
+    //TODO: can throw in the future
+    val name = taskArguments("name")
+    val otherArguments = taskArguments - "name"
+
     //TODO: can throw
     val moduleName = task.getAction.get.getModuleName
 
@@ -30,6 +35,11 @@ object TaskRenderer {
         .map({ case (k, v) => s"$k='$v'" })
         .mkString("\n")
 
-    taskArguments + (moduleName -> actionArgumentsString)
+    val orderedPairs = Seq(
+      "name" -> name,
+      moduleName -> actionArgumentsString
+    ) ++ otherArguments.toList.sorted
+
+    LinkedHashMap[String, Any](orderedPairs: _*)
   }
 }
