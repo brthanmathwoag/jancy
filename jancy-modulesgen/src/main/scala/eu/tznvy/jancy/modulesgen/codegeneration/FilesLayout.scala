e4e4f286208ae0e2e6e0e65605fd33a5293ce688
brthanmathwoag <brthanmathwoag@tznvy.eu>
2017-01-07 18:46:41 +0100

    Removed unused code

--- jancy-modulesgen/src/main/scala/eu/tznvy/jancy/modulesgen/codegeneration/FilesLayout.scala
+++ jancy-modulesgen/src/main/scala/eu/tznvy/jancy/modulesgen/codegeneration/FilesLayout.scala
@@ -5,8 +5,6 @@ import java.nio.file.{Files, Paths, Path, FileSystems}
 import eu.tznvy.jancy.modulesgen.model.ModuleMetadata
 
 class FilesLayout(srcRootPath: Path) {
-  def clean(): Unit = srcRootPath.toFile.delete()
-
   def saveModuleSource(moduleMetadata: ModuleMetadata, content: String): Unit = {
     val outputDirectory = srcRootPath.resolve(moduleMetadata.namespace.replace(".", FileSystems.getDefault.getSeparator))
     outputDirectory.toFile.mkdirs()
