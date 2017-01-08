8f7dc0f7ff7600eb59cf08531446464dbf148ffd
brthanmathwoag <brthanmathwoag@tznvy.eu>
2017-01-08 18:25:00 +0100

    Simplified FilesLayout code and added comments

--- jancy-modulesgen/src/main/scala/eu/tznvy/jancy/modulesgen/codegeneration/FilesLayout.scala
+++ jancy-modulesgen/src/main/scala/eu/tznvy/jancy/modulesgen/codegeneration/FilesLayout.scala
@@ -1,15 +1,21 @@
 package eu.tznvy.jancy.modulesgen.codegeneration
 
-import java.nio.file.{Files, Paths, Path, FileSystems}
+import java.nio.file.{Files, Paths, Path}
 
 import eu.tznvy.jancy.modulesgen.model.ModuleMetadata
 
+/**
+  * Saves module wrapper sourcecode in an appropriate package
+  * @param srcRootPath root sources path, e.g. jancy-modules/src/main/java
+  */
 class FilesLayout(srcRootPath: Path) {
+  //TODO: split path resolving logic and IO in two classes
   def saveModuleSource(moduleMetadata: ModuleMetadata, content: String): Unit = {
-    val outputDirectory = srcRootPath.resolve(moduleMetadata.namespace.replace(".", FileSystems.getDefault.getSeparator))
-    outputDirectory.toFile.mkdirs()
-
+    val pathComponents = moduleMetadata.namespace.split('.')
+    val outputDirectory = Paths.get(srcRootPath.toString, pathComponents: _*)
     val outputFile = outputDirectory.resolve(moduleMetadata.className + ".java")
+
+    Files.createDirectories(outputDirectory)
     Files.write(outputFile, content.getBytes)
   }
 }
